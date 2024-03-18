2a8b874ca06de03a378316dbe78a1692b65e0432
ronleeon <ronleeon@github.com>
2024-03-18 16:08:59 +0800

    fix: delete some useless headers now

--- test/main.cpp
+++ test/main.cpp
@@ -3,9 +3,6 @@
 #include "testBbTree.h"
 #include "ronleeon/tree/m_tree.h"
 #include "ronleeon/tree/B_tree.h"
-#include "ronleeon/misc/skip_list.h"
-#include "ronleeon/misc/union-find-set.h"
-#include "ronleeon/graph/graph.h"
 #include <vector>
 #include <set>
 
@@ -15,12 +12,5 @@ int main() {
     //testBTree();
     //testBbTree();
 	testSet();
-	// ronleeon::graph::DefaultGraph<int> g;
-	// auto Node1 = g.addNode(1);
-	// auto Node2 =g.addNode(2);
-	// auto Node3 =g.addNode(3);
-
-	// g.addEdge(Node1, Node2,12).addEdge(Node2,Node3,22).addEdge(Node1,Node4,33);
-	// g.bfs();
 	return 0;
 }
